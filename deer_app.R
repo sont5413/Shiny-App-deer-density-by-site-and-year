95a2abef26c916bac9c9d701a9acc5411808e55b
WillisSontheimer <95881308+sont5413@users.noreply.github.com>
2022-07-31 11:29:03 -0500

    Update deer_app.R

--- deer_app.R
+++ deer_app.R
@@ -16,10 +16,14 @@ server <- function(input, output) {
     deer_data %>%
       filter(site == input$site) %>%
   ggplot(aes(x=year,y=hicount)) +
-      geom_path() + 
-      geom_point() +
-      labs(title= "Deer Density by Year", 
-           y="Deer Density",x="Year")
+      geom_path(color = "grey", size = 0.5) + 
+      geom_point(shape=21, color="black", fill="#69b3a2", size=4) +
+      labs(title= "Deer Density Trends", 
+           y="Deer Density (acres per deer)",x="Year")+
+      theme(axis.text.x = element_text(size = 12),
+            axis.text.y = element_text(size = 12),
+            axis.title.x = element_text(size = 15),axis.title.y = element_text(size = 15))
+
   })
 }
 shinyApp(ui,server)
